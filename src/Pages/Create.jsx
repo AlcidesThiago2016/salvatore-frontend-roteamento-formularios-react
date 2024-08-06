3f378948a2ff3714249362bc15e39d45a4056d3b
Alcides Thiago <alcides_thiago@hotmail.com>
2024-08-06 17:27:09 +0000

    Api implemented in create

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,8 +1,37 @@
+import { useNavigate } from "react-router-dom"
+import { Api } from "../Api/api"
+import { toast } from "react-toastify"
+
 export default function Create(){
+
+    const navigate = useNavigate()
+
+    async function handleSubmit(event){
+        event.preventDefault()
+
+        const devmon = {
+            nome: event.target.nome.value,
+            imagem: event.target.imagem.value,
+            evoluiPara: event.target.evoluiPara.value
+        }
+
+        const apiUrl = Api.personagem.create()
+
+        const response = await Api.buildApiPostRequest(apiUrl, devmon)
+
+        if (response.ok){
+            toast.success('DevMon criado com Sucesso!')
+            navigate('/')
+        }else {
+            const body = await response.json()
+            toast.error('Erro ao. criar DevMon: ' + body.error)
+        }
+    }
+
     return (
         <div>
             <h1>Criação de DevMon</h1>
-            <form >
+            <form  onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="nome">Nome*:</label><br />
                     <input type="text" id="nome" name="nome" placeholder="Digite o nome."/>
@@ -15,7 +44,6 @@ export default function Create(){
                     <label htmlFor="nome">Evolui para (Opcional):</label><br />
                     <input type="text" id="evoluiPara" name="evoluiPara" placeholder="Insira o nome da Evolução."/>
                 </div>
-                { /* ... imagem, evolui para */}
                 <button type="submit">Criar</button>
                 <button type="reset">Reiniciar</button>
             </form>
